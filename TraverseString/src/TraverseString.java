6fac27662cf96899865dacf1001aed0a36d26295
Alan Lopez <alan@Alans-MBP.lan>
2024-02-14 11:49:55 -0500

    Finished

--- TraverseString/src/TraverseString.java
+++ TraverseString/src/TraverseString.java
@@ -5,12 +5,10 @@ public class TraverseString {
     public static void removeDuplicates(String input){
         Set<Character> uniqueSet = new LinkedHashSet<>();
 
-        // Traverse the string and add unique characters to the set
         for (char c : input.toCharArray()) {
             uniqueSet.add(c);
         }
 
-        // Create a string from the set of unique characters
         StringBuilder result = new StringBuilder();
         for (char uniqueChar : uniqueSet) {
             result.append(uniqueChar);
